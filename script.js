2b2b00d6799dbb38d68a2588009131171d97b7f8
matthias <meretzmatthias@yahoo.de>
2022-04-21 15:43:32 +0200

    Add option to choose name

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const Player = (number, name, color) =>{
-  return {number, name, color}
+const Player = (number, name) =>{
+  return {number, name}
 }
 
 const gameBoard = (()=>{
@@ -31,7 +31,8 @@ const displayController = (()=>{
   const cells = document.querySelectorAll(".cell")
   const instructions = document.querySelector(".instruction-text")
   const resetBtn = document.querySelector(".reset")
-
+  const board = document.querySelector(".board")
+  const form = document.querySelector(".start-form")
   const addCellListeners = () => {
     cells.forEach(cell => {
       cell.addEventListener("click", e => gameController.handleCellClick(e.target))
@@ -55,7 +56,11 @@ const displayController = (()=>{
   const renderInstruction = (text) =>{
     instructions.innerText = text
   }
-
+  const renderBoard = () =>{
+    form.style.display = "none"
+    board.style.display = "grid"
+    resetBtn.style.display = "inline"
+  }
   const reset = () =>{
     cells.forEach(cell => {
       cell.classList.remove("player1-bg")
@@ -63,22 +68,24 @@ const displayController = (()=>{
       cell.classList.remove("winning-cell")
     })
   }
-  return {addButtonListeners, addCellListeners, reset, fillCell, renderInstruction, highlightCombination}
+  return {renderBoard,addButtonListeners, addCellListeners, reset, fillCell, renderInstruction, highlightCombination}
 })()
 
 
 
 const gameController = (() => {
-  let players = [Player(1,"alice","red"), Player(2,"bob", "green")]
+  let players = []
   let cellsActive = true
 
-  const init = () => {
+  const init = (player1Name, player2Name) => {
+    players.push(Player(1,player1Name),Player(2,player2Name))
     displayController.addCellListeners()
     displayController.addButtonListeners()
     startGame()
   }
 
   const startGame = () => {
+    displayController.renderBoard()
     displayController.reset()
     gameBoard.reset()
     cellsActive = true
@@ -89,24 +96,21 @@ const gameController = (() => {
     players.push(players.shift())
   }
 
+  // return null if game isnt over
   const getWinCombination = () => {
     const board = gameBoard.getBoard()
     let winCombinations = [
       [0,1,2],[3,4,5],[6,7,8],
       [0,4,8],[2,4,6],[0,3,6],
-      [1,4,7],[2,5,8]
-    ]
+      [1,4,7],[2,5,8]]
     let winCombination = null
+
     winCombinations.forEach(row => {
       if(board[row[0]] && board[row[0]] == board[row[1]] && board[row[0]] == board[row[2]]){
-        gameOver = true
         winCombination = row
       }
     })
-    if(gameBoard.isFull()){
-      return "tie"
-    }
-    
+    if(gameBoard.isFull()) return "tie"
     return winCombination
   }
 
@@ -129,7 +133,17 @@ const gameController = (() => {
     displayController.highlightCombination(winCombo)
     cellsActive = false
   }
+
   return {init, handleCellClick, startGame}
 })()
 
-gameController.init()
+const startBtn = document.querySelector(".start")
+
+startBtn.addEventListener("click", e => {
+  e.preventDefault()
+  let player1Name = document.getElementById("player1-name").value
+  let player2Name = document.getElementById("player2-name").value
+  if(!player1Name) player1Name = "Player 1"
+  if(!player2Name) player2Name = "Player 2"
+  gameController.init(player1Name, player2Name)
+})
